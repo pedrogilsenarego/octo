ae314c9ff4913287cc5bb6e7070f37925a0b3f44
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-03-11 21:54:20 +0000

    a

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
@@ -7,20 +7,47 @@ import "swiper/css/navigation";
 import "swiper/css/scrollbar";
 import { EffectCoverflow, Pagination, Navigation } from "swiper";
 import "./styles.css";
-import { Colors } from "../../constants/pallete";
+import Panel from "./Panel";
 
 const Carousel = () => {
-  const Image =
-    "https://images.pexels.com/photos/786220/pexels-photo-786220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-  const Image1 =
-    "https://tse3.mm.bing.net/th?id=OIP.x9fjmSreFgFKaUIZsd7shwHaE5&pid=Api&P=0";
-  const Image2 =
-    "https://tse2.mm.bing.net/th?id=OIP.U0_SqaehHlBoIrQAOkoyCgHaEo&pid=Api&P=0";
-  const Image3 =
-    "https://tse1.mm.bing.net/th?id=OIP.O07HmnWRY3QL0G9fsj4bBQHaF6&pid=Api&P=0";
-  const Image4 =
-    "https://tse3.mm.bing.net/th?id=OIP.j5t6bb2Atl6Fuig5ZpRWMAHaEq&pid=Api&P=0";
-  const Image5 = "https://mcdn.wallpapersafari.com/medium/58/42/YNoPB6.jpg";
+  const data = [
+    {
+      image:
+        "https://images.pexels.com/photos/786220/pexels-photo-786220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      title: "Human Bag",
+      price: 34,
+    },
+    {
+      image:
+        "https://tse3.mm.bing.net/th?id=OIP.x9fjmSreFgFKaUIZsd7shwHaE5&pid=Api&P=0",
+      title: "Human tree Bag",
+      price: 344,
+    },
+    {
+      image:
+        "https://tse2.mm.bing.net/th?id=OIP.U0_SqaehHlBoIrQAOkoyCgHaEo&pid=Api&P=0",
+      title: "Human tree Bag",
+      price: 344,
+    },
+    {
+      image:
+        "https://tse1.mm.bing.net/th?id=OIP.O07HmnWRY3QL0G9fsj4bBQHaF6&pid=Api&P=0",
+      title: "Human tree Bag",
+      price: 344,
+    },
+    {
+      image:
+        "https://tse3.mm.bing.net/th?id=OIP.j5t6bb2Atl6Fuig5ZpRWMAHaEq&pid=Api&P=0",
+      title: "Human tree Bag",
+      price: 344,
+    },
+    {
+      image: "https://mcdn.wallpapersafari.com/medium/58/42/YNoPB6.jpg",
+      title: "Human tree Bag",
+      price: 344,
+    },
+  ];
+
   return (
     <Box className='box'>
       <Swiper
@@ -45,39 +72,14 @@ const Carousel = () => {
         slidesPerView={3}
         className='swiper_container'
       >
-        <SwiperSlide>
-          <Box
-            className='image'
-            display='flex'
-            flexDirection='column'
-            justifyContent='center'
-            alignItems='center'
-            style={{ backgroundImage: `url(${Image})` }}
-          >
-            <Typography
-              style={{ marginTop: "100%" }}
-              color={Colors.NEON_YELLOW}
-            >
-              Human Bag
-            </Typography>
-            <Typography color={Colors.NEON_YELLOW}>30</Typography>
-          </Box>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={Image1} alt='' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={Image2} alt='' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={Image3} alt='' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={Image4} alt='' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={Image5} alt='' />
-        </SwiperSlide>
+        {data.map((item, pos) => {
+          return (
+            <SwiperSlide key={pos}>
+              <Panel image={item.image} title={item.title} price={item.price} />
+            </SwiperSlide>
+          );
+        })}
+
         {/* <div className='slider-controller'>
         <div className='swiper-button-prev slider-arrow'>
           <h1>A</h1>
