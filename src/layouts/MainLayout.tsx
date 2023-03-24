66f7e592c92ce367b1e3a4befb7e3b53c53a4b52
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-03-24 15:43:38 +0000

    a

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,61 +1,13 @@
 import MenuBar from "../presentional/MenuBar";
-import { Box, Container, useMediaQuery, useTheme, Grid } from "@mui/material";
-import Label from "../assets/images/label.png";
-import { useNavigate } from "react-router-dom";
-import { ROUTE_PATHS } from "../constants/routes";
+
+
 
 export const MainLayout = (props: any) => {
-  const theme = useTheme();
-  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const navigate = useNavigate();
+
+
   return (
     <>
-      {!mobile ? (
-
-        <Grid
-          container
-          display='flex'
-          justifyContent='space-between'
-          alignItems='start'
-          style={{
-            top: 0,
-            width: "80%",
-            marginLeft: "180px",
-
-            position: "absolute",
-
-
-          }}
-        >
-          <Grid item>
-            <Box
-              onClick={() => navigate(ROUTE_PATHS.HOME)}
-              width='250px'
-              height='250px'
-              style={{
-                cursor: "pointer",
-                marginTop: "-24px",
-                marginLeft: "-22px",
-                backgroundPosition: "center center",
-                backgroundRepeat: "no-repeat",
-                backgroundImage: `url(${Label})`,
-                backgroundSize: "100%",
-              }}
-            ></Box>
-          </Grid>
-          <Grid item>
-            <MenuBar />
-          </Grid>
-        </Grid>
-
-
-      ) : (
-        <>
-          {" "}
-          <MenuBar />
-        </>
-      )
-      }
+      <MenuBar />
 
       {props.children}
     </>
--- src/presentional/MenuBar/index.tsx
+++ src/presentional/MenuBar/index.tsx
@@ -1,4 +1,4 @@
-import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Container, Grid, useMediaQuery, useTheme } from "@mui/material";
 import { i18n } from "../../translations/i18n";
 import { BiSearch } from "react-icons/bi";
 import { AiOutlineShopping } from "react-icons/ai";
@@ -11,56 +11,97 @@ import { ROUTE_PATHS } from "../../constants/routes";
 import BasicPopover from "../../components/Popover";
 import { useState } from "react";
 import CollectionPopover from "./CollectionPopover";
+import { useNavigate } from "react-router-dom";
+import Label from "../../assets/images/label.png";
 
 const MenuBar = () => {
   const theme = useTheme();
+  const navigate = useNavigate();
   const mobile = useMediaQuery(theme.breakpoints.down("sm"));
   const [anchorPopover, setAnchorPopover] = useState<any>(null);
 
   const renderLaptop = () => {
     return (
-      <>
-        <Box mt="20px" display='flex' columnGap='40px' alignItems='center' >
-          <Button
-            title={i18n.t("menuBar.about")}
-            link={ROUTE_PATHS.ABOUT}
-            setAnchorPopover={setAnchorPopover}
-          />
 
-          <Button
-            title={i18n.t("menuBar.collection")}
-            link={ROUTE_PATHS.COLLECTION}
-            setAnchorPopover={setAnchorPopover}
-            collection
-          />
+      <div
+        style={{
+          top: 0,
+          width: "100vw",
 
-          <Button
-            title={i18n.t("menuBar.store")}
-            link={ROUTE_PATHS.SHOP}
-            setAnchorPopover={setAnchorPopover}
-          />
-          <Box display='flex' columnGap='12px' alignItems='center'>
-            <BiSearch
-              size='25px'
-              color={Colors.PRETTY_CREAM}
-              style={{ cursor: "pointer" }}
-            />
-            <BsInstagram
-              size='25px'
-              color={Colors.PRETTY_CREAM}
-              style={{ cursor: "pointer" }}
-            />
-            <AiOutlineShopping
-              size='25px'
-              color={Colors.PRETTY_CREAM}
-              style={{ marginTop: "-3px", cursor: "pointer" }}
-            />
-          </Box>
-        </Box>
-        <BasicPopover anchorEl={anchorPopover} setAnchorEl={setAnchorPopover}>
-          <CollectionPopover />
-        </BasicPopover>
-      </>
+          position: "absolute",
+        }}
+      >
+        <Container maxWidth='xl'>
+          <Grid
+            container
+            display='flex'
+            justifyContent='space-between'
+            alignItems='start'
+          >
+            <Grid item>
+              <Box
+                onClick={() => navigate(ROUTE_PATHS.HOME)}
+                width='250px'
+                height='250px'
+                style={{
+                  cursor: "pointer",
+                  marginTop: "-24px",
+                  marginLeft: "-22px",
+                  backgroundPosition: "center center",
+                  backgroundRepeat: "no-repeat",
+                  backgroundImage: `url(${Label})`,
+                  backgroundSize: "100%",
+                }}
+              ></Box>
+            </Grid>
+            <Grid item>
+              <Box mt="20px" display='flex' columnGap='40px' alignItems='center' >
+                <Button
+                  title={i18n.t("menuBar.about")}
+                  link={ROUTE_PATHS.ABOUT}
+                  setAnchorPopover={setAnchorPopover}
+                />
+
+                <Button
+                  title={i18n.t("menuBar.collection")}
+                  link={ROUTE_PATHS.COLLECTION}
+                  setAnchorPopover={setAnchorPopover}
+                  collection
+                />
+
+                <Button
+                  title={i18n.t("menuBar.store")}
+                  link={ROUTE_PATHS.SHOP}
+                  setAnchorPopover={setAnchorPopover}
+                />
+                <Box display='flex' columnGap='12px' alignItems='center'>
+                  <BiSearch
+                    size='25px'
+                    color={Colors.PRETTY_CREAM}
+                    style={{ cursor: "pointer" }}
+                  />
+                  <BsInstagram
+                    size='25px'
+                    color={Colors.PRETTY_CREAM}
+                    style={{ cursor: "pointer" }}
+                  />
+                  <AiOutlineShopping
+                    size='25px'
+                    color={Colors.PRETTY_CREAM}
+                    style={{ marginTop: "-3px", cursor: "pointer" }}
+                  />
+                </Box>
+              </Box>
+              <BasicPopover anchorEl={anchorPopover} setAnchorEl={setAnchorPopover}>
+                <CollectionPopover />
+              </BasicPopover>
+
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+
+
     );
   };
 
