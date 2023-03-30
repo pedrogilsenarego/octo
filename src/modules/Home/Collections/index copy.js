cc16fff8f70624986611efc343e2b8ffc94fde6f
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-03-30 16:52:15 +0100

    a

--- src/modules/Home/Collections/index copy.js
+++ src/modules/Home/Collections/index copy.js
@@ -94,29 +94,17 @@ const Collections = () => {
                 ? "scale(1) translate(0%, 0%) "
                 : " scale(0.3) translate(-2000px, -1000px) perspective(800px) rotateX(50deg)",
             overflow: "hidden",
-            transition: "all 0.6s ease-out",
+            transition: "all 0.5s ease-in-out",
           }}
         >
           <Box id='image-track'>
             <img
-              style={{
-                transform:
-                  scrollToL === 2
-                    ? "translate(0px, 0px)"
-                    : "translate(0px, 60px)",
-              }}
               className='image'
               draggable={false}
               src='https://images.unsplash.com/photo-1679025027099-39bbd6cd168d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
               alt=''
             />
             <img
-              style={{
-                transform:
-                  scrollToL === 2
-                    ? "translate(0px, 0px)"
-                    : "translate(0px, -40px)",
-              }}
               className='image'
               draggable={false}
               src='https://images.unsplash.com/photo-1680122575933-d6e81ac79d7f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
@@ -125,48 +113,24 @@ const Collections = () => {
             <img
               className='image'
               draggable={false}
-              style={{
-                transform:
-                  scrollToL === 2
-                    ? "translate(0px, 0px)"
-                    : "translate(0px, 40px)",
-              }}
               src='https://plus.unsplash.com/premium_photo-1661963552599-34680420acc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
               alt=''
             />
             <img
               className='image'
               draggable={false}
-              style={{
-                transform:
-                  scrollToL === 2
-                    ? "translate(0px, 0px)"
-                    : "translate(0px, -40px)",
-              }}
               src='https://plus.unsplash.com/premium_photo-1673624401742-a96945931271?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
               alt=''
             />
             <img
               className='image'
               draggable={false}
-              style={{
-                transform:
-                  scrollToL === 2
-                    ? "translate(0px, 0px)"
-                    : "translate(0px, 40px)",
-              }}
               src='https://images.unsplash.com/photo-1680013995061-2d6d1f3ab417?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
               alt=''
             />
             <img
               className='image'
               draggable={false}
-              style={{
-                transform:
-                  scrollToL === 2
-                    ? "translate(0px, 0px)"
-                    : "translate(0px, -40px)",
-              }}
               src='https://images.unsplash.com/photo-1604537466158-719b1972feb8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
               alt=''
             />
