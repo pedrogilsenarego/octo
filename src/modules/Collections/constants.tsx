b7aa426bdda463f9815f55a32ba80e92b0a2490b
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-04-18 09:53:33 +0100

    a

--- src/modules/Collections/constants.tsx
+++ src/modules/Collections/constants.tsx
@@ -12,8 +12,8 @@ export const configNeonYellow: BlockProps = {
     cymk: [23, 0, 100, 0],
   },
   firstRow: [
-    "https://res.cloudinary.com/daantetcr/image/upload/v1681686420/Octo/collections/OCTO_SS23_280a_vpistr.webp",
-    "https://res.cloudinary.com/daantetcr/image/upload/v1681686420/Octo/collections/OCTO_SS23_287a_jk0bwf.webp",
+    "https://res.cloudinary.com/daantetcr/image/upload/v1681686420/Octo/collections/NeonYellow/OCTO_SS23_280a_vpistr.webp",
+    "https://res.cloudinary.com/daantetcr/image/upload/v1681686420/Octo/collections/NeonYellow/OCTO_SS23_287a_jk0bwf.webp",
   ],
   secondRow: {
     image:
@@ -35,10 +35,10 @@ export const configNeonYellow: BlockProps = {
     ),
   },
   thirdRow: [
-    "https://res.cloudinary.com/daantetcr/image/upload/v1681682852/Octo/collections/OCTO_SS23_007a_wkat6t.webp",
-    "https://res.cloudinary.com/daantetcr/image/upload/v1681682851/Octo/collections/OCTO_SS23_073a_nakdt6.webp",
-    "https://res.cloudinary.com/daantetcr/image/upload/v1681682852/Octo/collections/OCTO_SS23_039a_ers2yz.webp",
-    "https://res.cloudinary.com/daantetcr/image/upload/v1681682852/Octo/collections/OCTO_SS23_026a_u6hita.webp",
+    "https://res.cloudinary.com/daantetcr/image/upload/v1681682852/Octo/collections/NeonYellow/OCTO_SS23_007a_wkat6t.webp",
+    "https://res.cloudinary.com/daantetcr/image/upload/v1681682851/Octo/collections/NeonYellow/OCTO_SS23_073a_nakdt6.webp",
+    "https://res.cloudinary.com/daantetcr/image/upload/v1681682852/Octo/collections/NeonYellow/OCTO_SS23_039a_ers2yz.webp",
+    "https://res.cloudinary.com/daantetcr/image/upload/v1681682852/Octo/collections/NeonYellow/OCTO_SS23_026a_u6hita.webp",
   ],
 };
 
