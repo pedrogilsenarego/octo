e59a243fa1f5268ffa5cbd1a9cf54b8d596bb122
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-04-18 11:47:35 +0100

    a

---
+++ src/modules/Collections/Block/Carousel/index.tsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+
+interface Props {
+  thirdRow: string[];
+  rgb: [number, number, number];
+  gap: number;
+  padding: number;
+}
+
+const Carousel = ({ thirdRow, rgb, gap, padding }: Props) => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  console.log(thirdRow.length);
+  const handleNextClick = (direction: number) => {
+    if (direction === 1 && slideIndex > thirdRow.length - 5) return;
+
+    setSlideIndex((prevIndex) => (prevIndex + 1 * direction) % thirdRow.length);
+  };
+  const containerWidth = window.innerWidth - 2 * (window.innerWidth * padding);
+  const childWidth = (containerWidth - gap * 3) / 4;
+  const slideWidth = childWidth + gap;
+  const slideWidthPercentage = slideWidth / containerWidth * 100;
+  return (
+    <div
+      style={{
+        width: window.innerWidth - 2 * (window.innerWidth * padding),
+
+        overflow: "hidden",
+        position: "relative",
+      }}
+    >
+      {thirdRow.length > 4 && (
+        <>
+          <div
+            onClick={() => handleNextClick(1)}
+            style={{
+              zIndex: 1000,
+              position: "absolute",
+              right: "20px",
+              cursor: "pointer",
+              top: "50%",
+              transform: "translateY(-50%)",
+              bottom: 0,
+              width: "40px",
+              height: "40px",
+              borderRadius: "50%",
+              backgroundColor: `rgb(${rgb[0]},${rgb[1]},${rgb[2]}`,
+            }}
+          ></div>
+          <div
+            onClick={() => handleNextClick(-1)}
+            style={{
+              zIndex: 1000,
+              position: "absolute",
+              left: "20px",
+              cursor: "pointer",
+              top: "50%",
+              transform: "translateY(-50%)",
+              bottom: 0,
+              width: "40px",
+              height: "40px",
+              borderRadius: "50%",
+              backgroundColor: `rgb(${rgb[0]},${rgb[1]},${rgb[2]}`,
+            }}
+          ></div>
+        </>
+      )}
+
+
+      <div
+        style={{
+          columnGap: "5px",
+          position: "relative",
+          display: "flex",
+          justifyContent: "flex-start",
+          transform: `translateX(-${slideIndex * slideWidthPercentage}%)`,
+
+          transition: "transform 0.5s",
+        }}
+      >
+        {thirdRow?.map((item: string, pos: number) => {
+          return (
+            <div
+              key={pos}
+              style={{
+                flex: `0 0 ${childWidth}px`,
+                overflow: "hidden",
+                height: window.innerHeight * 0.55,
+              }}
+            >
+              <img
+                src={item}
+                alt=''
+                style={{ objectFit: "cover", height: "100%", width: "100%" }}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/modules/Collections/Block/index.tsx
+++ src/modules/Collections/Block/index.tsx
@@ -1,7 +1,7 @@
 import { Grid } from "@mui/material";
 import Pantone, { PantoneProps } from "./Pantone";
-import { useState } from "react";
-import { generalConstants } from "../../../constants/general";
+import Carousel from "./Carousel";
+
 
 export interface BlockProps {
   pantoneProps: PantoneProps;
@@ -14,11 +14,7 @@ export interface BlockProps {
 }
 
 const Block = ({ pantoneProps, firstRow, secondRow, thirdRow }: BlockProps) => {
-  const [slideIndex, setSlideIndex] = useState(0);
 
-  const handleNextClick = (direction: number) => {
-    setSlideIndex((prevIndex) => (prevIndex + 1 * direction) % thirdRow.length);
-  };
   return (
     <div
       style={{
@@ -60,7 +56,7 @@ const Block = ({ pantoneProps, firstRow, secondRow, thirdRow }: BlockProps) => {
         </Grid>
       </Grid>
 
-      <Grid container columnSpacing='5px' style={{}}>
+      {/* <Grid container columnSpacing='5px' style={{}}>
         <Grid
           item
           xs={6}
@@ -83,75 +79,8 @@ const Block = ({ pantoneProps, firstRow, secondRow, thirdRow }: BlockProps) => {
         >
           {secondRow.jsx}
         </Grid>
-      </Grid>
-      <div
-        style={{
-          width: window.innerWidth - 2 * (window.innerWidth * 0.07),
-
-          overflow: "hidden",
-          position: "relative",
-
-
-        }}
-      >
-        <div
-          onClick={() => handleNextClick(1)}
-          style={{
-            zIndex: 1000,
-            position: "absolute",
-            right: "20px",
-            cursor: "pointer",
-            top: "50%",
-            transform: "translateY(-50%)",
-            bottom: 0,
-            width: "40px",
-            height: "40px",
-            borderRadius: "50%",
-            backgroundColor: `rgb(${pantoneProps.rgb[0]},${pantoneProps.rgb[1]},${pantoneProps.rgb[2]}`,
-          }}
-        ></div>
-        <div
-          onClick={() => handleNextClick(-1)}
-          style={{
-            zIndex: 1000,
-            position: "absolute",
-            left: "20px",
-            cursor: "pointer",
-            top: "50%",
-            transform: "translateY(-50%)",
-            bottom: 0,
-            width: "40px",
-            height: "40px",
-            borderRadius: "50%",
-            backgroundColor: `rgb(${pantoneProps.rgb[0]},${pantoneProps.rgb[1]},${pantoneProps.rgb[2]}`,
-          }}
-        ></div>
-        <div style={{
-          columnGap: "5px", position: "relative", display: "flex",
-          justifyContent: "flex-start",
-          transform: `translateX(-${slideIndex * 25}%)`,
-          transition: "transform 0.5s",
-        }}>
-          {thirdRow?.map((item: string, pos: number) => {
-            return (
-              <div
-                key={pos}
-                style={{
-                  flex: "0 0 25%",
-                  overflow: "hidden",
-                  height: window.innerHeight * 0.55,
-                }}
-              >
-                <img
-                  src={item}
-                  alt=''
-                  style={{ objectFit: "cover", height: "100%", width: "100%" }}
-                />
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      </Grid> */}
+      <Carousel thirdRow={thirdRow} gap={5} rgb={pantoneProps.rgb} padding={0.07} />
     </div>
   );
 };
